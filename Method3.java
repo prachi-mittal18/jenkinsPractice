b23c82118d43f3261d30dec0f0adfdc288c4e215
Prachi Mittal <160820339+prachi-mittal18@users.noreply.github.com>
2026-02-10 15:00:45 +0530

    Create Method3.java

---
+++ Method3.java
@@ -0,0 +1,16 @@
+public class Method3 {
+    public static void main(String[] args) {
+     
+
+      BufferedReader br = new BufferedReader(new FileReader("input.txt"));
+int a = Integer.parseInt(br.readLine());
+int b = Integer.parseInt(br.readLine());
+
+
+
+        int result = Integer.parseInt(a) / Integer.parseInt(b);
+        System.out.println("Result = " + result);
+    }
+}
+
+
